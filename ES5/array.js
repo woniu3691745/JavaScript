ee6d5324b48b9e026cb400ec39e151a5f9b36eac
lidongliang <3691745@qq.com>
2017-05-18 22:17:05 +0800

    update

---
+++ ES5/array.js
@@ -0,0 +1,14 @@
+// 栈方法
+// push() 数组的尾部
+var colors = ['red', 'blue'];
+colors.push('brown');
+colors[3] = 'black';
+console.log(colors[2]);
+// pop() 返回数组最后一项
+var item = colors.pop();
+console.log(item); // black
+
+// 队列方法
+// shift() 前端
+
+// unshift() 在数组前端添加任意项并返回新数组的长度
--- ES5/function.js
+++ ES5/function.js
@@ -24,7 +24,7 @@ var factorial1 = (function f(num){
 })
 
 // 闭包：指有权限访问另一个函数作用域中的变量的函数。
-// 创建闭包常见方式：就是在一个函数内部创建另一个函数。
+// 当在函数内部定义了其他函数时，就创建了闭包。闭包有权访问包含函数内部的所有变量。
 
 // 当某个函数被调用时，会创建一个执行环境及相应的作用域链，然后，使用arguments和其他命名参数的值来初始化函数的活动对象。
 
@@ -34,3 +34,70 @@ var factorial1 = (function f(num){
 // 式不同，这一点可能不会那么明显。
 
 
+// 私有变量
+function MyObject () {
+    // 私有变量和私有函数
+    var privateVariable = 10;
+    function privateFunction () {
+        return false;
+    }
+    // 特权方法  ##能够在构造函数中定义特权方法，是因为特权方法作为闭包有权访问在构造函数中定义的所有变量和函数。
+    this.publicMethod = function () {
+        privateVariable++;
+        return privateFunction();
+    }
+}
+
+// 静态私有变量
+(function(){
+    // 私有变量和私有函数
+    var privateVariable = 10;
+    function privateFunction () {
+        return false;
+    }
+
+    // 构造函数  ## 初始化未声明的变量，总是会创建一个全局变量。
+    MyObject = function (){
+    };
+
+    // 公有/特权方法  ## 作为闭包，总是保存着对包含作用域的引用。
+    MyObject.prototype.publicMethod = function () {
+        privateVariable++;
+        return privateFunction ();
+    }
+})();
+
+// 模块模式
+var singleton = function () {
+    // 私有变量和函数
+    var privateVariable = 10;
+    function privateFunction () {
+        return false;
+    }
+    // 特权/公有发放和属性
+    return {
+        publicProperty: true,
+        publicMethod: function () {
+            privateVariable++;
+            return privateFunction();
+        }
+    }
+}();
+// 这个模块模式使用了一个返回对象的匿名函数。
+var application = function () {
+    // 私有变量和属性
+    var components = new Array();
+    // 初始化
+    components.push('1');
+    // 公用
+    return {
+        getComponentCount: function () {
+            return components.length;
+        },
+        registerComponent: function (components) {
+            if (typeof components == 'Object') {
+                components.push(components);
+            }
+        }
+    }
+}
