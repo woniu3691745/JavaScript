d78c4e17e49f58e67ec53e827c0a62ff9bc3630a
lidl <3691745@qq.com>
2017-06-12 10:50:45 +0800

    add substr and substring

--- ES5/array.js
+++ ES5/array.js
@@ -12,6 +12,9 @@ console.log(item); // black
 // shift() 前端
 // unshift() 在数组前端添加任意项并返回新数组的长度
 
+/**
+ * 返回一个新的数组
+ */
 let array = [1,2,3,4,5];
 let r = array.splice(0, 2); // 包含起始位置
 console.log(r)
@@ -19,3 +22,13 @@ console.log(r)
 let array1 = [1,2,3,4,5];
 let r1 = array1.slice(1, 2); // 不包含起始位置
 console.log(r1);
+
+
+/**
+ * 返回字符串
+ */
+let str = '123456';
+let s = str.substr(1, 3), // 包含结束位置
+    s1 = str.substring(1 ,3); // 不包含结束位置
+console.log(s);
+console.log(s1);
